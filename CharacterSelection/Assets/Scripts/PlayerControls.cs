005814b906b5f59dbe71f9616e5480fe3a49d1dd
curban2336 <curban841@gmail.com>
2022-02-26 16:25:35 -0500

    Controls improved
    gamepad and keyboard controls should be working now. I need to optimize it and tweak it because it is still a little finicky

--- CharacterSelection/Assets/Scripts/PlayerControls.cs
+++ CharacterSelection/Assets/Scripts/PlayerControls.cs
@@ -6,10 +6,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    private PlayerInput playerInput;
+    private InputAction jump;
+    private InputAction move;
+    private InputAction interact;
+    private InputAction sprint;
+    private Vector2 movementVector;
+
     public GameObject meter;
     public EnergyDrain drain;
     public GameObject player;
     public bool running;
+    public bool walking;
     public bool atStore;
     public bool isJumping;
     public GameObject prompt;
@@ -21,7 +29,18 @@ public class PlayerControls : MonoBehaviour
     public List<GameObject> tasks;
     public List<GameObject> activejobs;
 
-    //var gamepad;
+    void Awake()
+    {
+        //playerInput = GetComponent<PlayerInput>();
+        //jump = playerInput.actions["Jump"];
+        //jump.ReadValue<float>();
+        //move = playerInput.actions["Move"];
+        //move.ReadValue<float>();
+        //interact = playerInput.actions["Interaction"];
+        //interact.ReadValue<float>();
+        //sprint = playerInput.actions["Sprint"];
+        //sprint.ReadValue<float>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -40,100 +59,76 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!Keyboard.current.leftShiftKey.isPressed)
+        if (walking)
+        {
+            if (running)
+            {
+                float move = movementVector.x * 40 * Time.deltaTime;
+                player.GetComponent<Rigidbody2D>().velocity += new Vector2(move, 0);
+            }
+            else
+            {
+                float move = movementVector.x * 10 * Time.deltaTime;
+                player.GetComponent<Rigidbody2D>().velocity += new Vector2(move, 0);
+            }
+        }
+    }
+
+    public void OnMove(InputValue movementValue)
+    {
+        if (walking)
+        {
+            walking = false;
+            movementVector = Vector2.zero;
+        }
+        else
+        {
+            walking = true;
+            movementVector = movementValue.Get<Vector2>();
+        }
+    }
+
+    public void OnJump()
+    {
+        isJumping = true;
+        drain.currentEffort = EffortType.Jump;
+    }
+
+    public void OnInteraction()
+    {
+        if (atStore)
+        {
+            if (moneyCount >= store.GetComponent<food>().cost)
+            {
+                drain.currentFood = store.GetComponent<food>().foodEat;
+                drain.eat = true;
+                moneyCount -= store.GetComponent<food>().cost;
+            }
+        }
+    }
+
+    public void OnSprint()
+    {
+        if (running)
         {
             running = false;
-            //drain.currentEffort = EffortType.Walk;
-            if (Keyboard.current.leftArrowKey.isPressed)
-            {
-                drain.currentEffort = EffortType.Walk;
-                float translation = -1 * 10 * Time.deltaTime;
-                player.GetComponent<Rigidbody2D>().velocity += new Vector2(translation, 0);
-            }
-            else if (Keyboard.current.rightArrowKey.isPressed)
-            {
-                drain.currentEffort = EffortType.Walk;
-                float translation = 1 * 10 * Time.deltaTime;
-                player.GetComponent<Rigidbody2D>().velocity += new Vector2(translation, 0);
-            }
         }
-        else if (Keyboard.current.leftShiftKey.isPressed)
+        else if (walking)
         {
             running = true;
-            drain.currentEffort = EffortType.Run;
-            if (Keyboard.current.leftArrowKey.isPressed)
-            {
-                float translation = -1 * 40 * Time.deltaTime;
-                player.GetComponent<Rigidbody2D>().velocity += new Vector2(translation, 0);
-            }
-            else if (Keyboard.current.rightArrowKey.isPressed)
-            {
-                float translation = 1 * 40 * Time.deltaTime;
-                player.GetComponent<Rigidbody2D>().velocity += new Vector2(translation, 0);
-            }
         }
-        //else if (!gamepad.current.leftTrigger.isPressed)
-        //{
-        //    running = false;
-        //    drain.currentEffort = EffortType.Walk;
-        //    float move = gamepad.leftStick.x * 40 * Time.deltaTime;
-        //    player.GetComponent<Rigidbody2D>().velocity += new Vector2(move, 0);
-        //}
-        //else if (gamepad.current.leftTrigger.isPressed)
-        //{
-        //    running = true;
-        //    drain.currentEffort = EffortType.Run;
-        //    float move = gamepad.leftStick.x * 40 * Time.deltaTime;
-        //    player.GetComponent<Rigidbody2D>().velocity += new Vector2(move, 0);
-        //}
-
-        if (isJumping)
-        {
-            float translation = 12f;
-            player.GetComponent<Rigidbody2D>().velocity += new Vector2(0, translation);
-            isJumping = false;
-            drain.currentEffort = EffortType.None;
-        }
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
-        {
-            isJumping = true;
-            drain.currentEffort = EffortType.Jump;
-        }
-        //else if (gamepad.current.buttonSouth.wasPressedThisFrame)
-        //{
-        //    isJumping = true;
-        //    drain.currentEffort = EffortType.Jump;
-        //}
-
-        if (Keyboard.current.eKey.wasPressedThisFrame)
-        {
-            if (atStore)
-            {
-                if (moneyCount >= store.GetComponent<food>().cost)
-                {
-                    drain.currentFood = store.GetComponent<food>().foodEat;
-                    drain.eat = true;
-                    moneyCount -= store.GetComponent<food>().cost;
-                }
-            }
-        }
-        //else if (gamepad.current.buttonWest.wasPressedThisFrame)
-        //{
-        //    if (atStore)
-        //    {
-        //        if (moneyCount >= store.GetComponent<food>().cost)
-        //        {
-        //            drain.currentFood = store.GetComponent<food>().foodEat;
-        //            drain.eat = true;
-        //            moneyCount -= store.GetComponent<food>().cost;
-        //        }
-        //    }
-        //}
     }
 
     void FixedUpdate()
     {
         money.GetComponent<Text>().text = moneyCount.ToString();
+        if (isJumping)
+        {
+            float translation = 10f;
+            player.GetComponent<Rigidbody2D>().velocity += new Vector2(0, translation);
+            isJumping = false;
+            drain.currentEffort = EffortType.None;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
